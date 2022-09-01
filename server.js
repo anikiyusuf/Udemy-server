6640fef5040536245935378a00737b5bcf8b0ef0
Aniki Yusuf <anikiyusuf99@gmail.com>
2022-09-01 11:06:21 +0100

    I uncommented my codes in the server.js file

--- server.js
+++ server.js
@@ -2,18 +2,16 @@
 
 
 
-const express = require(express)
-const app = express()
+// const express = require(express)
+// const app = express()
 
-app.get('/', function(req,res) {
-    res.send('Hello World')
-})
-
-
-
-app.listen(3000)
+// app.get('/', function(req,res) {
+//     res.send('Hello World')
+// })
 
 
+
+// app.listen(3000)
 
 
 
@@ -32,48 +30,50 @@ app.listen(3000)
 
 
 
-// const http = require('http')
-// const fs = require('fs')
-// const port = 3000
-// const hostname = 'localhost'
+
+
+const http = require('http')
+const fs = require('fs')
+const port = 3000
+const hostname = 'localhost'
 
 
 
-// const server = http.createServer((req,res) => {
-//   res.setHeader('Context-type', 'text/html')
-//   let route = "./view/"
-//    switch(req.url){
-//     case '/':
-//         console.log("hello again ")
-//         route += "index.html";
-//         res.statusCode= 200
-//         break;
-//     case "/contact":
-//         route += "Contact.html";
-//         res.statusCode =200
-//         break;
+const server = http.createServer((req,res) => {
+  res.setHeader('Context-type', 'text/html')
+  let route = "./view/"
+   switch(req.url){
+    case '/':
+        console.log("hello again ")
+        route += "index.html";
+        res.statusCode= 200
+        break;
+    case "/contact":
+        route += "Contact.html";
+        res.statusCode =200
+        break;
 
-//         case "/contact-us":
-//             res.statusCode = 301,
-//         res.setHeader = ('Location ', '/Contact'),
-//         res.end()
-//         break;
-//         default:
-//             route += "404.html";
-//             res.statusCode = 404
-//             break
-//   }
+        case "/contact-us":
+            res.statusCode = 301,
+        res.setHeader = ('Location ', '/Contact'),
+        res.end()
+        break;
+        default:
+            route += "404.html";
+            res.statusCode = 404
+            break
+  }
 
-// fs.readFile(route, (err,data) =>{
-//     if(err){
-//         console.log(err);
-//         res.end()
-//     }else{
+fs.readFile(route, (err,data) =>{
+    if(err){
+        console.log(err);
+        res.end()
+    }else{
         
-//         res.end(data)
-//     }
-// })
-// })
+        res.end(data)
+    }
+})
+})
 
 
 
